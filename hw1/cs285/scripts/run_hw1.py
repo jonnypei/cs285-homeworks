17eabac107e065c4f5f599db078a645451a97309
Jonny Pei <jonnypei@berkeley.edu>
2023-09-11 19:10:57 -0700

    adjusted

--- hw1/cs285/scripts/run_hw1.py
+++ hw1/cs285/scripts/run_hw1.py
@@ -163,7 +163,7 @@ def run_training_loop(params):
           # HINT2: use np.random.permutation to sample random indices
           # HINT3: return corresponding data points from each array (i.e., not different indices from each array)
           # for imitation learning, we only need observations and actions.  
-          indices = np.random.permutation(len(replay_buffer.obs))[:params['train_batch_size']]
+          indices = np.random.permutation(params['train_batch_size'])
           ob_batch, ac_batch = replay_buffer.obs[indices], replay_buffer.acs[indices]
 
           # use the sampled data to train an agent
